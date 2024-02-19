2502768a40163182b2e33cb7aaac08b9cf770add
William Lima <williamlims@hotmail.com>
2024-02-18 21:21:14 -0300

    feat(main)!: change places and submit initial updates to the main file of the project

--- app/src/main/java/br/com/photos/ui/MainActivity.kt
+++ app/src/main/java/br/com/photos/ui/MainActivity.kt
@@ -1,17 +1,35 @@
 package br.com.photos.ui
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.photos.R
+import br.com.photos.adapter.PhotoAdapter
+import br.com.photos.adapter.PhotoImageAdapter
 import br.com.photos.databinding.ActivityMainBinding
+import br.com.photos.model.Photo
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val photoList: MutableList<Photo> = mutableListOf()
+    private val productAdapter: PhotoAdapter by lazy {
+        PhotoAdapter(this, photoList)
+    }
+    private val photoImageList: MutableList<Bitmap> = mutableListOf()
+    private val photoImageAdapter: PhotoImageAdapter by lazy {
+        PhotoImageAdapter(this, photoImageList)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        setSupportActionBar(amb.mainTb.apply {
+            title = getString(R.string.app_name)
+        })
+
+
 
     }
 }
